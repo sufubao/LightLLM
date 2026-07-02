36d65f849b0a809367003769bd1aafa9568f344c
wangzaijun <wzjhelloworld@qq.com>
2026-07-02 07:39:13 +0000

    fix

--- lightllm/server/router/model_infer/mode_backend/chunked_prefill/impl.py
+++ lightllm/server/router/model_infer/mode_backend/chunked_prefill/impl.py
@@ -357,6 +357,7 @@ class ChunkedPrefillBackend(ModeBackend):
         all_next_token_ids.append(next_token_ids)
         # process the draft model output
         for draft_model_idx in range(self.mtp_step):
+
             draft_model_input.input_ids = draft_next_token_ids
             draft_model_input.mtp_draft_input_hiddens = draft_model_output.mtp_main_output_hiddens
             # spec decode: MTP
@@ -398,6 +399,7 @@ class ChunkedPrefillBackend(ModeBackend):
         all_next_token_ids.append(next_token_ids)
         # process the draft model output
         for _step in range(self.mtp_step):
+
             draft_model_input.input_ids = draft_next_token_ids
             draft_model_input.mtp_draft_input_hiddens = draft_model_output.mtp_main_output_hiddens
             # spec decode: MTP
